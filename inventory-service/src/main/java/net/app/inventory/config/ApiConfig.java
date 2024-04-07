ff69edffa0adc24bc944cae1288b135156622248
Anish Panthi <anish.panthi@gmail.com>
2024-04-07 13:27:44 -0500

    Added CLR to add new inventory and order

--- inventory-service/src/main/java/net/app/inventory/config/ApiConfig.java
+++ inventory-service/src/main/java/net/app/inventory/config/ApiConfig.java
@@ -2,6 +2,11 @@ package net.app.inventory.config;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import net.app.inventory.dto.InventoryDto;
+import net.app.inventory.service.InventoryService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +14,21 @@ import org.springframework.context.annotation.Configuration;
  * @author Anish Panthi
  */
 @Configuration
-public class ApiConfig {
+@RequiredArgsConstructor
+public class ApiConfig implements CommandLineRunner {
+
+  private final InventoryService inventoryService;
+
+  @Override
+  public void run(String... args) throws Exception {
+    if (inventoryService.findAll().isEmpty()) {
+      inventoryService.create(
+          new InventoryDto(null, "Pixel 8 Pro", BigDecimal.valueOf(899.00), 100, 4));
+    }
+  }
 
   @Bean
   ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
     return new ObservedAspect(observationRegistry);
   }
-
 }
--- order-service/src/main/java/net/app/order/config/ApiConfig.java
+++ order-service/src/main/java/net/app/order/config/ApiConfig.java
@@ -2,6 +2,11 @@ package net.app.order.config;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import net.app.order.dto.OrderDto;
+import net.app.order.service.OrderService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +14,24 @@ import org.springframework.context.annotation.Configuration;
  * @author Anish Panthi
  */
 @Configuration
-public class ApiConfig {
+@RequiredArgsConstructor
+public class ApiConfig implements CommandLineRunner {
+
+  private final OrderService orderService;
 
   @Bean
   ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
     return new ObservedAspect(observationRegistry);
+  }
+
+  /**
+   * @param args
+   */
+  @Override
+  public void run(String... args) {
+    if (orderService.findAll().isEmpty()) {
+      orderService.create(new OrderDto(null, "Pixel 8 Pro", BigDecimal.valueOf(899.00), 1,
+          BigDecimal.valueOf(899.00 * 1)));
+    }
   }
 }
